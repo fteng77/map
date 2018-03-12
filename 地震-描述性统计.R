6a334d101e90cec38dbcdf21812da0966eb07122
fteng77 <fteng77@126.com>
2018-03-12 22:22:59 +0800

    修改

--- 地震-描述性统计.R
+++ 地震-描述性统计.R
@@ -14,11 +14,11 @@ dizhen<-read.csv("dizhen.all.new2.csv",header=T,stringsAsFactors = F)
 dizhen<-dizhen[,-1] #删掉第一列
 dizhen<-mutate(dizhen,earthquake.mag=zhenji)  #为了显得高大上，把zhenji改为earthquake.mag
 #dizhen<-filter(dizhen,year>=1970&year<2018)
+qplot(factor(year),zhenji,data=dizhen,geom="boxplot",color="black")+theme(axis.text.x = element_text(size=7,angle=45))+theme(legend.position = "none")
 
-
-dizhen3<-filter(dizhen,zhenji>=3) #三级以上的地震
-dizhen4<-filter(dizhen,zhenji>=4) #四级以上的地震
-dizhen5<-filter(dizhen,zhenji>=5) #五级以上的地震
+# dizhen3<-filter(dizhen,zhenji>=3) #三级以上的地震
+# dizhen4<-filter(dizhen,zhenji>=4) #四级以上的地震
+# dizhen5<-filter(dizhen,zhenji>=5) #五级以上的地震
 
 dizhen.nosheng<-filter(dizhen,is.na(as.numeric(sheng))==F) #提取没有省名的记录
 max(dizhen.nosheng$zhenji)
@@ -41,12 +41,20 @@ nosheng.plot
 dizhen.year<-filter(dizhen,is.na(year)==F&year!=2018)
 qplot(as.factor(year),zhenji,data=dizhen.year,geom="boxplot",xlab="年份",ylab="发生次数")
 #qplot(as.factor(year),zhenji,data=dizhen.year,geom="jitter",alpha=I(1/10),color=I("red"),xlab="年份",ylab="发生次数")
-qplot(as.factor(year),data=dizhen.year,geom="bar",xlab="年份",ylab="发生次数")
-dizhen.year.3<-filter(dizhen.year,zhenji>=5)
-qplot(factor(year),data=dizhen.year.3,geom="bar",xlab="年份",ylab="发生次数")
+qplot(as.factor(year),data=dizhen.year,geom="bar",xlab="年份",ylab="发生次数")+labs(title="zheji")+theme(axis.text.x = element_text(size=7,angle=45))  
+qplot(as.factor(year),data=dizhen.year,geom="bar",xlab="年份",ylab="发生次数")+theme(axis.text.x = element_text(size=7,angle=45))  
 
+dizhen.year.3<-filter(dizhen.year,zhenji>=3)
+dizhen.year.4<-filter(dizhen.year,zhenji>=4)
+dizhen.year.5<-filter(dizhen.year,zhenji>=5)
+dizhen.year.6<-filter(dizhen.year,zhenji>=6)
 
+(x1<-qplot(factor(year),data=dizhen.year.3,geom="bar",xlab="年份",ylab="震级超过3级的发生次数")+theme(axis.text.x = element_text(size=7,angle=45)) )
+(x2<-qplot(factor(year),data=dizhen.year.4,geom="bar",xlab="年份",ylab="震级超过4级的发生次数")+theme(axis.text.x = element_text(size=7,angle=45)) )
+(x3<-qplot(factor(year),data=dizhen.year.5,geom="bar",xlab="年份",ylab="震级超过5级的发生次数")+theme(axis.text.x = element_text(size=7,angle=45)) )
+(x4<-qplot(factor(year),data=dizhen.year.6,geom="bar",xlab="年份",ylab="震级超过6级的发生次数")+theme(axis.text.x = element_text(size=7,angle=45)) )
 
+qplot(zhenji,data=dizhen.year.4,geom="histogram",xlab="")+ facet_wrap( ~ factor(year), ncol=10)#分页，参考：http://www.mamicode.com/info-detail-1272791.html
 
 
 
