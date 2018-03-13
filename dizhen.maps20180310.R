fb7dbd78664999f794744203c440dcf0ed96cfaf
fteng77 <fteng77@126.com>
2018-03-13 08:24:22 +0800

    0813

--- dizhen.maps20180310.R
+++ dizhen.maps20180310.R
@@ -6,31 +6,23 @@ library(maps)
 library(mapproj)
 library(plyr)
 library(tidyverse)
-setwd("D:/dizhen")
 
 ####1. 中国地图，参考https://zhuanlan.zhihu.com/p/26708368
-china_map <- readShapePoly("bou2_4p.shp")       # 读取地图空间数据
+china_map <- readShapePoly("D:/dizhen/bou2_4p.shp")       # 读取地图空间数据
 china<- fortify(china_map)
 china.plot<-ggplot()+
   geom_polygon(data=china, aes(x=long, y=lat, group=group),fill="grey95", colour="grey60",size=0.25)+
   coord_map("polyconic")
 
+setwd("D:/dizhen")
 dizhen<-read.csv("dizhen.all.new2.csv",header=T,stringsAsFactors = F)
 dizhen<-dizhen[,-1]
-dizhen<-mutate(dizhen,magnitude=zhenji)
 dizhen3<-filter(dizhen,zhenji>=3)
 dizhen4<-filter(dizhen,zhenji>=4)
 dizhen5<-filter(dizhen,zhenji>=5)
-zhenji.hist<-hist(dizhen$magnitude,main="histogram of magnitude",xlab="magnitude")
+zhenji.hist<-hist(dizhen$zhenji)
 
-china.plot+
-  geom_point(data=dizhen5,aes(x=jingdu,y=weidu,size=magnitude),alpha=I(1/5),shape=I(21),color=I("black"),fill=I("black"))+
-  labs(x = NULL, y = NULL)+
-  theme(legend.position = "bottom")
-
-#scale_colour_hue("what does it eat?");
-#theme(legend.position =c(120,5))
-#scale
+china.plot+geom_point(data=dizhen4,aes(x=jingdu,y=weidu,size=zhenji,color=zhenji,fill=I("blue")),alpha=I(1/12),shape=I(21))
 #reference: geom_point(data=china_data, aes(x = jd,y = wd),size=4,fill="black",  colour="white")+ #散点图
 
 ####2. 分省地图，参考http://blog.sina.com.cn/s/blog_6bc5205e0102vmgq.html
@@ -54,17 +46,17 @@ weifang.plot<-ggplot()+
   coord_map("polyconic")
 
 zhejiang.web<-"https://raw.githubusercontent.com/fteng77/dizhen/master/zj.dizhen.fujin.csv"
-zhejiang.dizhen<-filter(dizhen,sheng=="浙江省")
-zhejiang.plot+geom_point(data=zhejiang.dizhen,aes(x=jingdu,y=weidu,size=magnitude),fill=I("red"),alpha=I(1/5),shape=21,colour=I("red"))
+zhejiang.dizhen<-read.csv(zhejiang.web,header=T,stringsAsFactors = F)
+zhejiang.plot+geom_point(data=zhejiang.dizhen,aes(x=jingdu,y=weidu,size=zhenji),fill=I("red"),alpha=I(1/5),shape=21,colour=I("red"))
 
 ningbo.dizhen<-filter(zhejiang.dizhen,shi=="宁波市")
-ningbo.plot+geom_point(data=ningbo.dizhen,aes(x=jingdu,y=weidu,size=magnitude),fill="red",alpha=0.8,shape=21,colour="red")
+ningbo.plot+geom_point(data=ningbo.dizhen,aes(x=jingdu,y=weidu,size=zhenji),fill="red",alpha=0.8,shape=21,colour="red")
 
 weifang<-subset(china_map_data,NAME_2=="Weifang")
 weifang.plot<-ggplot()+
   geom_polygon(data=weifang, aes(x=long, y=lat, group=group),fill="grey95", colour="grey60",size=0.25)+
   coord_map("polyconic")
-weifang.dizhen<-filter(dizhen,shi=="潍坊市")
-weifang.plot+geom_point(data=weifang.dizhen,aes(x=jingdu,y=weidu,size=magnitude),fill="red",alpha=I(1/2),shape=21,colour="red")
+weifang.dizhen<-filter(dizhen,shi.1=="潍坊市")
+weifang.plot+geom_point(data=weifang.dizhen,aes(x=jingdu,y=weidu,size=zhenji),fill="red",alpha=0.8,shape=21,colour="red")
 
 ggsave("D:/dizhen/ningbo.png",width=5,height = 5)
